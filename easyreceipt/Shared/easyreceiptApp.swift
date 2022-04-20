25a67c9eb713b1df86a45ba20032d18b080eb34a
andre pradhana <apt@cathedral-oaks.local>
2022-04-20 07:49:19 -0700

    EasyReceipt from xcode.

---
+++ easyreceipt/Shared/easyreceiptApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyreceiptApp.swift
+//  Shared
+//
+//  Created by andre pradhana on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct easyreceiptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
