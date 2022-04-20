9d55d90e2c61d386408e7385b5b94b02b8197c36
andre pradhana <apt@cathedral-oaks.local>
2022-04-20 07:58:15 -0700

    Add more elements to ContentView.

--- easyreceipt/Shared/ContentView.swift
+++ easyreceipt/Shared/ContentView.swift
@@ -2,20 +2,30 @@
 //  ContentView.swift
 //  Shared
 //
-//  Created by andre pradhana on 4/20/22.
+//  Created by jeremiah choi, ap on 4/19/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+        ScrollView {
+            Text("Test hello world")
+        }.navigationBarTitle("Easy Receipt")
+        .navigationBarItems(trailing: Button(action: {
+             print("Fetching json data")
+        }, label: {
+           Text("Fetch")
+        }))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
