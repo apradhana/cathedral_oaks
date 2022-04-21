9435c1f2d72f1f24c1d0eca8e13355eecd1000a3
andre pradhana <apt@cathedral-oaks.local>
2022-04-20 20:33:15 -0700

    WIP: Adding more details based on the youtube video.

--- easyreceipt/Shared/ContentView.swift
+++ easyreceipt/Shared/ContentView.swift
@@ -7,11 +7,40 @@
 
 import SwiftUI
 
+let apiUrl = "https://jsonplaceholder.typicode.com/todos"
+
+struct ReceiptData: Identifiable, Decodable {
+    let id = UUID()
+    let name: String
+}
+
+class ReceiptDataViewModel: ObservableObject {
+
+    @Published var messages = "Messages inside observable object"
+
+    @Published var courses: [Course] = {
+        .init(name: "Receipt 1"),
+        .init(name: "Receipt 2"),
+        .init(name: "Receipt 3")
+    }
+
+    func changeMessage() {
+        self.messages = "blah blah blah"
+    }
+
+    func fetchCourses() {
+        guard let url = URL(string: apiUrl) else {return}
+        URLSession.shared.dataTask(with:url) { (data, resp, err) in
+        
+        self.courses = try! JSONDecoder().decode([Course].self, from: data!)}.resume()
+    }
+
+}
 struct ContentView: View {
     var body: some View {
         NavigationView {
         ScrollView {
-            Text("Test hello world")
+            Text("Test hello world!")
         }.navigationBarTitle("Easy Receipt")
         .navigationBarItems(trailing: Button(action: {
              print("Fetching json data")
---
+++ jeremiahawesomeapp/Shared/jeremiahawesomeappApp.swift
@@ -0,0 +1,18 @@
+//
+//  jeremiahawesomeappApp.swift
+//  Shared
+//
+//  Created by andre pradhana on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct jeremiahawesomeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+#imageLiteral(resourceName: "simulator_screenshot_9045AB2D-52A3-4510-9C3B-450529DE9701.png")
